bae73bb3cae64acfef759ee1758693946fe66c73
davboecki <dboeken@yahoo.de>
2015-04-16 20:02:59 +0200

    Add Task 2, isset.

--- Makefile
+++ Makefile
@@ -2,14 +2,17 @@ CC = gcc
 
 .PHONY: all default clean
 
-default: myecho myenv
+default: myecho myenv isset
 
-all: myecho myenv
+all: myecho myenv isset
 
 myecho: myecho.c
 
 myenv: myenv.c
 
+isset: isset.c
+
 clean:
 	rm -f myecho
 	rm -f myenv
+	rm -f isset
---
+++ isset.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[], char* envp[]) {
+	if(argc < 2) {
+		printf("ERROR: Too few arguments");
+		return -1;
+	}
+	if(argc > 3) {
+		printf("ERROR: Too many arguments");
+		return -1;
+	}
+	char* name = NULL;
+	int showResult = 0;
+	if(argc == 3) {
+		if(strcmp(argv[1], "-v")) {
+			name = argv[2];
+		} else if(strcmp(argv[2], "-v")) {
+			name = argv[1];
+		} else {
+			printf("ERROR: Wrong arguments");
+			return -1;
+		}
+		showResult = 1;
+	} else {
+		name = argv[1];
+	}
+	char* result = getenv(argv[1]);
+	if(showResult && result != NULL) {
+		printf("%s\n", result);
+	}
+	return result == NULL ? 1 : 0;
+}
